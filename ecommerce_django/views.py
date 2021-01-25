213dc31fa49d390f1287c1bc60f371ae1590349f
Luis Giglio <luisfgiglio@gmail.com>
2021-01-25 16:50:45 -0300

    Erro

---
+++ ecommerce_django/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+    context = {
+    'title': 'Página inicial',
+    'content': 'Bem-vindo a página inicial'
+    }    
+    return render(request, 'home_page.html', context)
+
+def about_page(request):
+    context = {
+    'title': 'Página sobre',
+    'content': 'Bem-vindo a página sobre'
+    }
+    return render(request, 'about/view.html', context)
+
+def contact_page(request):
+    context = {
+    'title': 'Página de contato',
+    'content': 'Bem-vindo a página contatos'
+    }
+    return render(request, 'contact/view.html', context)
+
